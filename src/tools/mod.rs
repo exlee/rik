14e66fed43925dc749fb11a6c1b97c91d0aa1322
Przemysław Alexander Kamiński <exlee@users.noreply.github.com>
2026-05-29 10:31:44 +0200

    Add moodify tool stub with Mood enum

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,10 +1,12 @@
 pub mod complete_marker;
 pub mod edit_file;
 pub mod list_files;
+pub mod moodify;
 pub mod read_file;
 pub mod write_file;
 
 pub use complete_marker::find_markers;
 pub use edit_file::EditFileTool;
 pub use list_files::ListFilesTool;
+pub use moodify::{Mood, MoodifyTool};
 pub use read_file::ReadFileTool;
